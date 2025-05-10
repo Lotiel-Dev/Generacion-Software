d2b375cbaa1d24d9eb01a04ad1ab6665392a0132
Frosmin <simonabastomart@gmail.com>
2025-05-10 18:24:13 -0400

    feat: agregar restricciones de not null y unique a los campos FirstName, LastName y Email en el modelo User

--- backend/models/User.go
+++ backend/models/User.go
@@ -6,8 +6,8 @@ type User struct {
 	gorm.Model
 
 	Username  string `grorm:"unique;not null"`
-	FirstName string
-	LastName  string
-	Email     string
+	FirstName string `grorm:"not null"`
+	LastName  string `grorm:"not null"`
+	Email     string `grorm:"unique;not null"`
 	Password  string
 }
