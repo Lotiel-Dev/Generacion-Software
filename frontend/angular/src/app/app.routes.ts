c2bc7563c0af9cf21226ae01dcd8655171d411c0
Frosmin <simonabastomart@gmail.com>
2025-05-12 19:18:26 -0400

    feat: crear componente Home y ItemList, actualizar rutas y estilos

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
+import { ItemListComponent } from './shared/item-list/item-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
+  { path: '', component: HomeComponent },
+
+
+  // listas
+  {path: "lista-videos", component: ItemListComponent}
 ];
