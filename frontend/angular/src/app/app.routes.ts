b718d567fe862082adaf35a19abffdd479510ae1
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-12 21:22:44 -0400

    add: ruta a componente de lista de ejercicios

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ItemListComponent } from './shared/item-list/item-list.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ExercisesListComponent } from './pages/exercises-list/exercises-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
 
 
   // listas
-  {path: "lista-videos", component: ItemListComponent}
+  {path: "lista-videos", component: ItemListComponent},
+  {path: "lista-ejercicios", component: ExercisesListComponent}
 ];
