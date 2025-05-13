f8d19ce5f838aa1e5e1aa20e6b3611e57c931753
Josuex123 <josue7pedro@gmail.com>
2025-05-13 12:34:21 -0400

    fix: descargar ahora esta en cursos

--- frontend/angular/src/app/pages/courses-list/courses-list.component.ts
+++ frontend/angular/src/app/pages/courses-list/courses-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import jsPDF from 'jspdf';
 @Component({
   selector: 'app-courses-list',
   imports: [],
@@ -7,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrl: './courses-list.component.scss'
 })
 export class CoursesListComponent {
+   leccionTitulo = 'Lección 1';
+  leccionContenido = 'Este es el contenido de la lección que se verá en el archivo.';
 
+   descargarComoTxt(): void {
+    const texto = document.getElementById('lorem-ipsum-text')?.innerText;
+    
+    if (texto) {
+      const blob = new Blob([texto], { type: 'text/plain' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'lorem-ipsum.txt'; 
+      a.click();
+      URL.revokeObjectURL(url);
+    }
+  }
+ descargarComoPDF(): void {
+  const texto = document.getElementById('lorem-ipsum-text')?.innerText;
+
+  if (texto) {
+    const doc = new jsPDF();
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const margin = 10;
+    const usableWidth = pageWidth - margin * 2;
+
+    doc.setFontSize(16);
+    doc.text('Texto de ejemplo:', margin, 20);
+
+    doc.setFontSize(12);
+
+    const lineas = doc.splitTextToSize(texto, usableWidth);
+
+    doc.text(lineas, margin, 30);
+
+    doc.save('lorem-ipsum.pdf');
+  }
+}
 }
--- frontend/angular/src/app/pages/exercises-list/exercises-list.component.ts
+++ frontend/angular/src/app/pages/exercises-list/exercises-list.component.ts
@@ -9,42 +9,6 @@ import jsPDF from 'jspdf';
 })
 
 export class ExercisesListComponent {
-   leccionTitulo = 'Lección 1';
-  leccionContenido = 'Este es el contenido de la lección que se verá en el archivo.';
 
-   descargarComoTxt(): void {
-    const texto = document.getElementById('lorem-ipsum-text')?.innerText;
-    
-    if (texto) {
-      const blob = new Blob([texto], { type: 'text/plain' });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'lorem-ipsum.txt'; 
-      a.click();
-      URL.revokeObjectURL(url);
-    }
-  }
- descargarComoPDF(): void {
-  const texto = document.getElementById('lorem-ipsum-text')?.innerText;
-
-  if (texto) {
-    const doc = new jsPDF();
-    const pageWidth = doc.internal.pageSize.getWidth();
-    const margin = 10;
-    const usableWidth = pageWidth - margin * 2;
-
-    doc.setFontSize(16);
-    doc.text('Texto de ejemplo:', margin, 20);
-
-    doc.setFontSize(12);
-
-    const lineas = doc.splitTextToSize(texto, usableWidth);
-
-    doc.text(lineas, margin, 30);
-
-    doc.save('lorem-ipsum.pdf');
-  }
-}
  
 }
