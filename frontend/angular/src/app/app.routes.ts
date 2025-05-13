8dcb12c1ef1a2b6ae469119a9d50b7fcbd755891
Josuex123 <josue7pedro@gmail.com>
2025-05-13 12:32:28 -0400

    chore: ruta creada

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ExercisesListComponent } from './pages/exercises-list/exercises-list.component';
 import { VideosListComponent } from './pages/videos-list/videos-list.component';
-//import { CoursesListComponent } from './pages/courses-list/courses-list.component';
+import { CoursesListComponent } from './pages/courses-list/courses-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,5 +11,5 @@ export const routes: Routes = [
   // listas
   {path: "lista-videos", component: VideosListComponent},
   {path: "lista-ejercicios", component: ExercisesListComponent},
-  //{path: "lista-cursos", component: CoursesListComponent}
+  {path: "lista-cursos", component: CoursesListComponent}
 ];
