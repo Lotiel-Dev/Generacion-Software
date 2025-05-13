98ef5653f1d59b9ad031dda567914d1d555fef69
Frosmin <simonabastomart@gmail.com>
2025-05-12 23:17:37 -0400

    feat: actualizar rutas y componentes de listas, eliminar ItemListComponent

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { ItemListComponent } from './shared/item-list/item-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ExercisesListComponent } from './pages/exercises-list/exercises-list.component';
+import { VideosListComponent } from './pages/videos-list/videos-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
 
 
   // listas
-  {path: "lista-videos", component: ItemListComponent},
+  {path: "lista-videos", component: VideosListComponent},
   {path: "lista-ejercicios", component: ExercisesListComponent}
 ];
