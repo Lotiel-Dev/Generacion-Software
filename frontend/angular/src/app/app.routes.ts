a731e7ae26251efbbaad8c777890058db16e664b
Javier Nina Paco <keima13234@hotmail.com>
2025-05-13 13:24:44 -0400

    Subida de Buscador

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ExercisesListComponent } from './pages/exercises-list/exercises-list.component';
 import { VideosListComponent } from './pages/videos-list/videos-list.component';
-
+import {PaginaBuscadorComponent} from './pages/pagina-buscador/pagina-buscador.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
 
 
   // listas
+  { path: 'buscador', component: PaginaBuscadorComponent },
   {path: "lista-videos", component: VideosListComponent},
   {path: "lista-ejercicios", component: ExercisesListComponent}
 ];
