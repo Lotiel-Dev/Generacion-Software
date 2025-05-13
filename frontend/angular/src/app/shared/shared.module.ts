df3bf5204e82f508a4363c6d39062d0885eb6e4a
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-05-13 00:55:10 -0400

    chore: add folder structure for editor and forms module

--- frontend/angular/src/app/shared/shared.module.ts
+++ frontend/angular/src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule } from '@angular/forms';  // <<--- Agrega esto
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EditorComponent,],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    EditorComponent,
+    // exporta también NavbarComponent, FooterComponent si los usas fuera
   ]
 })
 export class SharedModule { }
+
+
