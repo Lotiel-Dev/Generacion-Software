b06620d5fc7daf39badd9f2aa632f14f8151bf03
Frosmin <simonabastomart@gmail.com>
2025-05-16 16:28:30 -0400

    refactor: mejorar la estructura del componente de lista de videos y agregar carga de datos

--- frontend/angular/src/app/pages/videos-list/videos-list.component.ts
+++ frontend/angular/src/app/pages/videos-list/videos-list.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Subscriber } from 'rxjs';
+
+interface Video {
+  ID: number;
+  Title: String;
+  Link: string;
+  Level: number;
+}
 
 @Component({
   selector: 'app-videos-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './videos-list.component.html',
-  styleUrl: './videos-list.component.scss'
+  styleUrl: './videos-list.component.scss',
 })
-export class VideosListComponent {
+export class VideosListComponent implements OnInit {
+  videos: Video[] = [];
+  filteredVideos: Video[] = [];
+  currentLevel: number = 1;
 
+    constructor(private http: HttpClient){}
+
+    ngOnInit(): void {
+      this.loadVideos();
+    }
+
+    loadVideos(): void {
+      this.http.get<Video[]>('/api/videos').subscribe({
+        next : (response) => {
+          this.videos = response;
+          this.filterVideosByLevel();
+        },
+        error: (error) => {
+          console.error('error al cargar el video', error);
+        }
+      });
+    }
+  filterVideosByLevel(): void {
+    this.filteredVideos = this.videos.filter(video => video.Level === this.currentLevel);
+  }
+
+  changeLevel(level: number): void {
+    this.currentLevel = level;
+    this.filterVideosByLevel();
+  }
 }
