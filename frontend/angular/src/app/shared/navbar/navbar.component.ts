fe041668777271536230d28781890a85af1cf557
Frosmin <simonabastomart@gmail.com>
2025-05-16 22:57:49 -0400

    feat: agregar efecto de desplazamiento en la barra de navegación

--- frontend/angular/src/app/shared/navbar/navbar.component.ts
+++ frontend/angular/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TerminalComponent } from '../../pages/terminal/terminal.component';
@@ -8,9 +8,17 @@ import { TerminalComponent } from '../../pages/terminal/terminal.component';
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive, TerminalComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isScrolled = false;
+  ngOnInit(): void {
+    this.checkScroll();
+  }
 
+  @HostListener('window:scroll',[])
+    checkScroll(){
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      this.isScrolled = scrollPosition > 100;
+    }
 }
-
