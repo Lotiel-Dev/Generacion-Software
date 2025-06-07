26b6db66bc537bcaf915c87c000c394ad3ea75b7
erwinparedez <erwin.erwin.ep@gmail.com>
2025-06-07 00:02:10 -0400

    Quitar el navbar y el footer al estar en el path del login

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +16,14 @@ import { FooterComponent } from './shared/footer/footer.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular';
+  showLayout = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const cleanUrl = event.urlAfterRedirects || event.url;
+        this.showLayout = !cleanUrl.includes('/login');
+      });
+  }
 }
