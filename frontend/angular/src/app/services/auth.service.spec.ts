53eabcf61c154d8b78ee827b797f98522175df4a
Carlos Antonio Gutierrez Copara <karlos817gerceis@gmail.com>
2025-06-08 20:36:32 -0400

    Corrección de la prueba de formato de fecha y mejora de las pruebas unitarias de AuthService con las importaciones y proveedores necesarios.

--- frontend/angular/src/app/services/auth.service.spec.ts
+++ frontend/angular/src/app/services/auth.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        AuthService,
+        {
+          provide: Router,
+          useValue: { navigate: jasmine.createSpy('navigate') }
+        }
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
